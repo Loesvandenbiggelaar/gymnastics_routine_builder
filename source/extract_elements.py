2f8ee0994e04d08b6ffe29a24862bd4ae4fe0e10
loesvandenbiggelaar <vdbiggelaarloes@gmail.com>
2024-02-08 17:50:39 +0100

    update the script so it uses a class.

--- source/extract_elements.py
+++ source/extract_elements.py
@@ -1,163 +1,157 @@
-import PyPDF2
 import re
+import PyPDF2
 import yaml
-import json
+from difflib import SequenceMatcher
 
 
-def loadConfig(file):
-    """
-    Load the config file, which is in YAML format.
 
-    file: The YAML file to read
-    """
-    with open(file, "r") as f:
-        y = yaml.safe_load(f)
-    return y
+class elementExtractor:
+    def __init__(self, config, language) -> None:
 
+        self.config = self.loadConfig(config)
+        self.language = language
+        self.reader = self.loadReader()
+        self.apparatuses = []
+        self.elements = {}
+        self.groups = {}
 
-def loadPages(file):
-    """
-    Load all the elements from specific pages. for each page a new pandas dataframe is created.
-
-    file: PDF input file
-    pages: page numbers from the PDF to read.
-    """
-
-    print("reading pages of", file)
-    return PyPDF2.PdfReader(file)
-
-
-def extractElements(s, apparatus):
-    if apparatus == "vault":
-        pattern = (
-            r"(T?\d\.\d+?)\s+(?!TOEGEVOEGD?)(?!T\d\.\d+)([^\d \ ].+?)\s*(\d\.\d*)\s*P"
-        )
-    elif apparatus == "uneven bars":
-        pattern = (
-            r"(\d+\.\d+)\s+(?!(?:Brug|\d\.\d{3,}|\s|–|-|\.|\/))(.*?)(?=\s+\d+\.\d+|$|\s{8,})"
-        )
-    elif apparatus == "beam":
-        pattern = (
-            r"(\d+\.\d+)\s+(?!(?:Balk|\d\.\d{3,}|\s|–|-|\.|\/))(.*?)(?=\s+\d+\.\d+|$|\s{8,})"
-        )
-    elif apparatus == "floor":
-        pattern = (
-            r"(\d+\.\d+)\s+(?!(?:Vloer|\d\.\d{3,}|\s|–|-|\.|\/))(.*?)(?=\s+\d+\.\d+|$|\s{4,})"
-        )
-    else:
-        raise ValueError("apparatus name is not valid")
-    return re.findall(pattern, s)
+        return
+    
+    def loadConfig(self, config):
+        """
+        Load the config file, which is in YAML format.
+        file: The YAML file to read
+        """
+        print("Loading config file")
+        with open(config, "r") as f:
+            return yaml.safe_load(f)
 
 
-def findGroup(element):
-    return element[0].split(".")[0]
+    def loadReader(self):
+        """
+        Load the pdf file
+        """
+        file =  self.config["file"][self.language]
+        print("reading pages of", file)
+        return PyPDF2.PdfReader(file)
+    
 
+    def addApparatus(self, app):
+        """
+        add a apparatus to analyze.
+        """
+        def add(a):
+            if a in self.apparatuses:
+                print(a, "is already added")
+            else:
+                self.apparatuses.append(a)
+                self.groups[a] = {}
+                self.elements[a] = []
+            return
+        
+        if type(app) == str:
+            add(app)
+        elif type(app) == list:
+            for a in app:
+                add(a)
+        else: 
+            print("type not supported")
+        return
+       
+    def getGroup(self, page, apparatus):
+        """
+        Return the number and name of the group on the given page.
+        also convert a string to an integer. 
+        since the pdf converter is not perfect, double spaces can be introduced which need to be corrected for.
+        """
 
-def writeResult(file, elements):
-    with open(file, "w", encoding="utf-8") as f:
-        f.write("number\tdescription\tvalue\tgroup\tdifficulty\n")
-        for element in elements:
-            f.write(
-                "\t".join([e if e != None else " " for e in element]).replace("\r", " ")
-                + "\n"
-            )
+        regex = self.config["apparatuses"][apparatus]["regex"]["group"][self.language]
+        for res in re.findall(regex, page):
+            number = int(float(res[0]))
+            name =self.adjustString(res[1])
+            if number in self.groups[apparatus].keys():
+                ratio = SequenceMatcher(None, name.lower(), self.groups[apparatus][number]).ratio()
+                if ratio < 0.9:
+                    print(name.lower())
+                    print(self.groups[apparatus][number])
+                    print(page)
+                    raise AssertionError ("items are not similar enough:", ratio)
+            else:                
+                self.groups[apparatus][number] = name.lower()
+        
+        return
 
-def writeGroups(file, groups):
-    with open(file, "w") as f:
-        json.dump(groups, f)
+    def getElements(self, page, apparatus):
+        regex = self.config["apparatuses"][apparatus]["regex"]["element"][self.language]
+        res = re.findall(regex, page)
+        for r in res:
+            self.elements[apparatus].append(list(r))
+        return 
 
-def addDifficulty(element):
-    difficulty = ["TA", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-    n = element[0].split(".")[1]
-    if len(n) == 3:
-        return difficulty[int(n[0])]
-    elif len(n) == 4:
-        return difficulty[int(n[0:2])]
-    else:
-        raise ValueError("No valid number")
+    def loadPages(self, apparatus):
+        """
+        load the pages from the reader for the apparatus. The pages are defined in the config file. 
+        """
+        pages_conf =  self.config["apparatuses"][apparatus]
+        return self.reader.pages[pages_conf["start page"] : pages_conf["end page"]]
 
+    def adjustString(self, s):
+        """Do string replacements listed in the config file."""
 
-def addValue(difficulty):
-    if difficulty in ["SA", "TA", "A"]:
-        return 0.1
-    elif difficulty == "B":
-        return 0.2
-    elif difficulty == "C":
-        return 0.3
-    elif difficulty == "D":
-        return 0.4
-    elif difficulty == "E":
-        return 0.5
-    elif difficulty == "F":
-        return 0.6
-    elif difficulty == "G":
-        return 0.7
-    elif difficulty == "H":
-        return 0.8
-    elif difficulty == "I":
-        return 0.9
-    elif difficulty == "J":
-        return 1.0
-    else:
-        raise ValueError("please enter a valid difficulty (in capitals).")
+        for adj in self.config["string adjustments"]:
+            s = s.replace(adj[0], adj[1])
+        return s
+    
+    def processApparatuses(self):
+        for apparatus in self.apparatuses:
+            pages = self.loadPages(apparatus)
+            for page in pages:
+                text = page.extract_text().replace("\n", "")
+                self.getGroup(text, apparatus)
+                self.getElements(text, apparatus)
+        
+    
+    def writeResult(self):
+        for apparatus, elements in self.elements.items():
+            # print(apparatus)
+            # print(elements)
+            with open(self.config["output directory"] + apparatus + ".tsv", "w", encoding="utf-8") as f:
+                # f.write("number\tdescription\tvalue\tgroup\tdifficulty\n")
+                for element in elements:
+                    # print(element)
+                    f.write(
+                        "\t".join([e if e != None else " " for e in element]).replace("\r", " ")
+                        + "\n"
+                    )
 
-def getGroups(page, apparatus):
-    """
-    Return the number and name of the group on the given page.
-    also convert a string to an integer. 
-    since the pdf converter is not perfect, double spaces can be introduced which need to be corrected for.
-    """
-    if apparatus == "vault":
-        pattern = r"GROEP\s+(\d)\s+.\s+(.*?)\s+\d\.\d{2}"
-    else:
-        pattern = r"(\d\.000)\s+.\s+(([A-Z\’\.\s]+(?:[A-Z]{2,}|\.)))"
-    res = re.findall(pattern, page)
-    assert len(res) >= 1, ("oops, something went wrong", page)
-    return [int(float(res[0][0])), res[0][1].replace("  ", " ").replace(" ’", "’").replace("ME T", "MET").replace("MET /", "MET/").replace("MET/ ", "MET/").replace("FLIK -FLAK", "FLIK-FLAK").replace("LA -DRAAI", "LA-DRAAI")]
+    def expandElements(self):
+        def getDifficulty(element):
+            difficulty = ["TA", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+            n = element[0].split(".")[1]
+            if len(n) == 3 or len(n) == 2:
+                return difficulty[int(n[0])]
+            elif len(n) == 4:
+                return difficulty[int(n[0:2])]
+            else:
+                raise ValueError("No valid number:", element)
+        for apparatus, elements in self.elements.items():
+            for element in elements:
+                number = element[0]
+                grNumber = int(number.split(".")[0].replace("T", ""))
+                element.append(str(grNumber))
 
+                difficulty = getDifficulty(element)
+                element.append(difficulty)
+                # print(self.groups)
+                # group = self.groups[apparatus][grNumber]
+                
+                pass
 
 def main():
-    file = "pages_config.yaml"
-    config = loadConfig(file)
-    reader = loadPages(config["FIG"])
-    groups = {}
-    for apparatus in config["apparatuses"]:
-        print("Processing", apparatus)
-        groups[apparatus] = {}
-        elements = []
-        pages = reader.pages[
-            config["pages"][apparatus]["start"] : config["pages"][apparatus]["end"]
-        ]
-        for page in pages:
-            text = page.extract_text().replace("\n", "")
-            number, name= getGroups(text, apparatus)
-            if number in groups[apparatus].keys():
-                # this is a weird exception where they mean the same but write it differently
-                if name == "HANDSTANDOVERSLAG MET/ZONDER 1/1 DRAAI IN 1E VLUCHTFASE - IN 2E VLUCHTFASE SALTO VOOR - OF ACHTEROVER MET/ZONDER LA-DRAAI":
-                    continue
-                assert groups[apparatus][number] == name.lower(), "whoops "
-
-
-            else:                
-                groups[apparatus][number] = name.lower()
-
-
-
-            elements_page = extractElements(text, apparatus)
-
-            for element in elements_page:
-                if apparatus == "vault":
-                    elements += [list(element) + [findGroup(element)] + [" "]]
-                else:
-                    difficulty = addDifficulty(element)
-                    value = str(addValue(difficulty))
-                    elements += [
-                        [element[0], element[1].replace("  ", " "), value, findGroup(element),difficulty]
-                    ]
-
-        writeResult(config["output_directory"] + apparatus + ".tsv", elements)
-    writeGroups(config["output_directory"] + "groups.json", groups)
-
-
+    extractor = elementExtractor("pages_config.yaml", language="nl")
+    extractor.addApparatus(["vault","uneven bars", "beam", "floor"])
+    extractor.processApparatuses()
+    extractor.expandElements()
+    extractor.writeResult()
 if __name__ == main():
     main()
