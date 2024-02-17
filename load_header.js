9209c42084a93ca836e4f06d04374413c30ec6fb
tbollen <thomas.bollen97@gmail.com>
2024-02-17 13:10:54 +0100

    Append to <body> instead of div with id

--- load_header.js
+++ load_header.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    $('#header-container').load('header.html');
+    // Fetch the content of header.html
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            // Append the fetched content to the existing content inside #header-container
+            $('body').prepend(data);
+        })
+        .catch(error => console.error('Error fetching header:', error));
 });
