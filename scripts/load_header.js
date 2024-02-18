f6aab78a3af5361a40c889e9de4a5f5d01131f9f
loesvandenbiggelaar <vdbiggelaarloes@gmail.com>
2024-02-18 17:15:48 +0100

    create "routine builder" page and set header for all pages

--- scripts/load_header.js
+++ scripts/load_header.js
@@ -1,16 +1,15 @@
-
-$(document).ready(async function() {    
-    try {
-        // Fetch the content of header.html
-        const response = await fetch('header.html');
-        if (!response.ok) {
-            throw new Error('Failed to fetch header: ' + response.status);
-        }
-        const data = await response.text();
-
-        // Append the fetched content to the existing content inside #header-container
-        $('body').prepend(data);
-    } catch (error) {
-        console.error('Error fetching header:', error);
+$(document).ready(async function () {
+  try {
+    // Fetch the content of header.html
+    const response = await fetch("header.html");
+    if (!response.ok) {
+      throw new Error("Failed to fetch header: " + response.status);
     }
+    const data = await response.text();
+
+    // Append the fetched content to the existing content inside #header-container
+    $("body").prepend(data);
+  } catch (error) {
+    console.error("Error fetching header:", error);
+  }
 });
