3f23c55c59abc82dd4a80712258f51e55e53cdd3
tbollen <thomas.bollen97@gmail.com>
2024-02-29 11:47:43 +0100

    Replaced default Scrollbar for Svrollbar component

--- routine-builder/src/routes/+layout.svelte
+++ routine-builder/src/routes/+layout.svelte
@@ -1,136 +1,140 @@
 <!-- SCRIPT -->
 <script>
-    // Import Global Styles
-    import '$lib/global.css'
+	// Animation
+	import { fly } from 'svelte/transition';
+	// Import Global Styles
+	import '$lib/global.css';
 
-    // Iconify for cool icons!
-    import Icon from '@iconify/svelte';
-    // Import to check routing URL
-    import { page } from '$app/stores';
-    const routes = [
-        { name: 'Elements', path: '/elements', icon: 'mdi:magnify-scan' },
-        { name: 'Routine Builder', path: '/routine_builder', icon: 'material-symbols:sports-gymnastics' },
-        { name: 'Rules', path: '/rules', icon: 'material-symbols:book-4' },
-    ];
-    // Set currentPath to see if the link is active
-    $: current_path = $page.route.id;
+	// Iconify for cool icons!
+	import Icon from '@iconify/svelte';
+	// Scrollbar
+	import { Svrollbar } from 'svrollbar';
 
-    // Title of current page
-    let title = 'Gymnastics Bible';
+	// Import to check routing URL
+	import { page } from '$app/stores';
+	const routes = [
+		{ name: 'Elements', path: '/elements', icon: 'mdi:magnify-scan' },
+		{
+			name: 'Routine Builder',
+			path: '/routine_builder',
+			icon: 'material-symbols:sports-gymnastics'
+		},
+		{ name: 'Rules', path: '/rules', icon: 'material-symbols:book-4' }
+	];
+	// Set currentPath to see if the link is active
+	$: current_path = $page.route.id;
 
-    //Set title for tabs (somehow it only works with a function?)
-    $: current_path_name = routes.find(route => route.path === current_path).name;
-    
-    function set_tab_title(title, path) {
-        return (path === undefined) ? title : ['GB', current_path_name].join(" - ");
-    }
+	// Title of current page
+	let title = 'Gymnastics Bible';
 
-    $: tab_title = set_tab_title(title, current_path_name);
+	//Set title for tabs (somehow it only works with a function?)
+	$: current_path_name = routes.find((route) => route.path === current_path).name;
 
-    // Current Language
-    $: current_lang = 'en';
+	function set_tab_title(title, path) {
+		return path === undefined ? title : ['GB', current_path_name].join(' - ');
+	}
+
+	$: tab_title = set_tab_title(title, current_path_name);
+
+	// Current Language
+	$: current_lang = 'en';
 </script>
 
 <!-- HEADER COMPONENT -->
 <title>{tab_title}</title>
+<Svrollbar />
 <header id="header">
-  <a href="/" id="header_logo">{title}</a>
-  <nav>
-    {#each routes as route}
-      <a
-      class="page_nav"
-      href={route.path}
-      class:active={current_path == route.path}>
-        <Icon icon={route.icon} />
-        {route.name}
-    </a>
-    {/each}
-  </nav>
-  <nav id="language_selector">
-    {current_lang}
-    <Icon icon="material-symbols:language" />
-  </nav>
+	<a href="/" id="header_logo">{title}</a>
+	<nav>
+		{#each routes as route}
+			<a class="page_nav" href={route.path} class:active={current_path == route.path}>
+				<Icon icon={route.icon} />
+				{route.name}
+			</a>
+		{/each}
+	</nav>
+	<nav id="language_selector">
+		{current_lang}
+		<Icon icon="material-symbols:language" />
+	</nav>
 </header>
-  <slot />
+<slot />
 
 <!-- STYLES -->
 <style>
-header {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    gap: 20px;
-    align-items: center;
+	header {
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		gap: 20px;
+		align-items: center;
 
-    /* Sticky Header */
-    position: sticky;
-    top: 0;
+		/* Sticky Header */
+		position: sticky;
+		top: 0;
 
-    background: var(--color-accent-gradient);
-    padding: 10px var(--page-margin-side);
-}
+		background: var(--color-accent-gradient);
+		padding: 10px var(--page-margin-side);
+	}
 
-#header_logo {
-    text-transform: lowercase;
-    text-decoration: none;
-    color: var(--color-attention);
-}
+	#header_logo {
+		text-transform: lowercase;
+		text-decoration: none;
+		color: var(--color-attention);
+	}
 
-nav {
-    justify-self: flex-end;
-    display: flex;
-    gap: 18px;
-    font-family: Kode Mono;
-}
+	nav {
+		justify-self: flex-end;
+		display: flex;
+		gap: 18px;
+		font-family: Kode Mono;
+	}
 
-nav a {
-    /* Aligning the icons */
-    display: flex;
-    align-items: center;
-    gap: 0.2em;
+	nav a {
+		/* Aligning the icons */
+		display: flex;
+		align-items: center;
+		gap: 0.2em;
 
-    /* Positioning */
-    position: relative;
-    padding: 0.3em;
+		/* Positioning */
+		position: relative;
+		padding: 0.3em;
 
-    /* text style */
-    font-size: 1.2em;
-    font-weight: 300;
-    text-transform: lowercase;
-    text-decoration: none;
-    color: inherit;
-}
+		/* text style */
+		font-size: 1.2em;
+		font-weight: 300;
+		text-transform: lowercase;
+		text-decoration: none;
+		color: inherit;
+	}
 
-iconify-icon {
-    font-size: 1.5em;
-}
+	nav a.active {
+		font-weight: bold;
+	}
 
-nav a.active {
-    font-weight: bold;
-}
+	nav a::after {
+		content: '';
+		position: absolute;
+		height: 2px;
+		bottom: 0;
+		left: 0;
+		width: 0px;
+		background-color: var(--color-text);
+		transition: width 0.25s ease;
+	}
 
-nav a::after {
-    content: '';
-    position: absolute;
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    width: 0px;
-    background-color: var(--color-text);
-    transition: width 0.25s ease;
-}
+	nav a.active::after,
+	nav a:hover::after {
+		width: 100%;
+	}
 
-nav a.active::after, nav a:hover::after {
-    width: 100%;
-}
+	#language_selector {
+		/* Aligning the icons */
+		display: flex;
+		align-items: center;
+		gap: 0.2em;
 
-#language_selector {
-    /* Aligning the icons */
-    display: flex;
-    align-items: center;
-    gap: 0.2em;
-
-    /* Hover */
-    cursor: pointer;
-}
+		/* Hover */
+		cursor: pointer;
+	}
 </style>
