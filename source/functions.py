3fa8f27831b6ba3b998104abca39506becfa1373
loesvandenbiggelaar <vdbiggelaarloes@gmail.com>
2024-03-02 15:07:13 +0100

    Support english language.

--- source/functions.py
+++ source/functions.py
@@ -1,5 +1,6 @@
 import yaml
 import json
+import fitz
 
 def loadConfig(file):
     """
@@ -21,4 +22,26 @@ def saveJson(file, elements):
     with open(file, "w") as f:
         f.write(json.dumps(elements))
 
-        
+def loadPages(file, page_range):
+    pdf = fitz.open(file)
+        # Check if the target page is within the range of pages
+    # Select the given target pages
+    if isinstance(page_range, int):
+        if page_range < 0 or page_range >= len(pdf):
+            print("Error: Invalid target page number.")
+            return
+        pages = [page_range]
+    elif isinstance(page_range, list):
+        pages = range(page_range[0], page_range[1]+1)
+    else:
+        raise ValueError ("target_page not valid")
+    print("page range:", pages)
+    return [pdf.load_page(i -1) for i in pages]
+
+
+def adjustString(s, adjustments):
+    """Do string replacements listed in the config file."""
+
+    for adj in adjustments:
+        s = s.replace(adj[0], adj[1])
+    return s
