7c40c6400771ecc55e8747b64aa14a4db8b45291
loesvandenbiggelaar <vdbiggelaarloes@gmail.com>
2024-03-20 21:05:54 +0100

    replace inputs with select2. Start counting the nr of elements

--- scripts/routine_builder.js
+++ scripts/routine_builder.js
@@ -17,13 +17,21 @@ load_elements("data/elements.json");
 // The data is fetched and is filtered on the selected apparatus.
 // Then, for each group (1 t/m 6) an optgroup is created
 // TODO: might need to change this into a nice plugin.
-function create_datalist() {
+
+// When a correct input is given, another empty slot needs to be created so the user can keep adding inputs.
+// A counter is used to keep track of the number of elements/slots.
+function create_datalist(div) {
   // do HTML stuff
-  datalist = document.createElement("datalist");
-  datalist.setAttribute("id", "elements_datalist");
+
+  input_div = document.createElement("div");
+  input_div.setAttribute("id", "input_div_" + globalThis.counter);
   select = document.createElement("select");
-  select.setAttribute("style", "width:100px");
-  datalist.append(select);
+  input_div.append(select);
+  select.setAttribute("style", "width:80%");
+  select.setAttribute("id", "selector_" + globalThis.counter);
+  select.setAttribute("multiple", "multiple");
+  select.setAttribute("change", "update_routine(this)");
+  div.append(input_div);
 
   // fetch data
   data = globalThis.elements_data[globalThis.selected_apparatus];
@@ -32,23 +40,30 @@ function create_datalist() {
   // TODO: fetch the groups based on the metadata/element group info, instead of a random list with numbers.
   ["1", "2", "3", "4", "5", "6"].forEach((group) => {
     optgroup = document.createElement("optgroup");
-    optgroup.setAttribute("label", group);
     select.append(optgroup);
     data.forEach((element) => {
       if (element["group"] == group) {
         option = document.createElement("option");
-        option.setAttribute("value", element["description"]);
+        option.setAttribute("value", element["number"]);
         // setting the innerhtml is a intermediate solution for showing the group and value of an element.
         if (globalThis.selected_apparatus == "vault") {
-          option.innerHTML = "GR " + group + "; val " + element["value"];
+          option.innerHTML =
+            "(" + element["value"] + " p.) " + element["description"];
         } else {
-          option.innerHTML = "GR " + group + "; dif " + element["difficulty"];
+          option.innerHTML =
+            "(" + element["difficulty"] + ") " + element["description"];
         }
         optgroup.append(option);
       }
     });
   });
-  return datalist;
+  selector = $("#selector_" + globalThis.counter);
+  selector.select2();
+  selector.on("change.select2", function (e) {
+    update_routine();
+  });
+
+  globalThis.counter += 1;
 }
 
 // This function will be executed when clicked on one of the apparatus buttons.
@@ -60,8 +75,7 @@ function showApparatus() {
     globalThis.counter = 1;
     div = $("#routine_placeholder");
     div.empty();
-    div.append(create_datalist());
-    add_input();
+    create_datalist(div);
     update_d_score();
   } else {
     alert("Please select an option.");
@@ -70,25 +84,13 @@ function showApparatus() {
 
 // When a correct input is given, another empty slot needs to be created so the user can keep adding inputs.
 // A counter is used to keep track of the number of elements/slots.
-function add_input() {
-  div = $("#routine_placeholder");
-  search_input = document.createElement("input");
-  search_input.setAttribute("type", "text");
-  search_input.setAttribute("placeholder", "element " + globalThis.counter);
-  // search_input.setAttribute("data-optgroup-list", "elements_datalist"); // does not work somehow
-  search_input.setAttribute("list", "elements_datalist");
-  search_input.setAttribute("id", "element_" + globalThis.counter);
-  search_input.setAttribute("onKeyUp", "update_routine(this)");
-  div.append(search_input);
-  globalThis.counter += 1;
-}
 
 // Given a string of text (the input), find the correct entry in the 'database'.
 function find_element(string) {
   data = globalThis.elements_data[globalThis.selected_apparatus];
   res = {};
   globalThis.elements_data[globalThis.selected_apparatus].forEach((element) => {
-    if (element["description"] == string) {
+    if (element["number"] == string) {
       res = element;
     }
   });
@@ -100,31 +102,40 @@ function find_element(string) {
 function update_d_score() {
   // reset values so we can start counting.
   globalThis.moeilijkheidswaarde = 0;
-  nr_elements = 1;
+  nr_elements = 0;
 
   for (let index = 1; index < globalThis.counter; index++) {
-    element = $("#element_" + String(index)).val();
-    res = find_element(element);
-    if (res["value"]) {
-      globalThis.moeilijkheidswaarde +=
-        Math.round(parseFloat(res["value"]) * 100) / 100;
-    }
+    selector_data = $("#selector_" + index).select2("data");
+    selector_data.forEach((element) => {
+      res = find_element(element["id"]);
+      if (res["value"]) {
+        globalThis.moeilijkheidswaarde += parseFloat(res["value"]);
+        nr_elements += 1;
+      }
+    });
   }
 
   // update the HTML
-  $("#mw_value").html(globalThis.moeilijkheidswaarde);
+  $("#mw_value").html(
+    Math.round((globalThis.moeilijkheidswaarde + Number.EPSILON) * 100) / 100
+  );
+  $("#nr_elements").html(nr_elements);
 }
 
 // if the second last input is empty or incorrect, the last input can be removed.
 // Otherwise we can end up with a lot of empty slots!
 function delete_input_if_necessary() {
-  if (globalThis.counter > 2) {
-    element_description = $("#element_" + String(globalThis.counter - 2)).val();
-    res = find_element(element_description);
-    if (!res["description"]) {
-      $("#element_" + String(globalThis.counter - 1)).remove();
-      globalThis.counter -= 1;
+  selector_data = $("#selector_" + String(globalThis.counter - 2));
+  found = false;
+  selector_data.select2("data").forEach((element) => {
+    res = find_element(element["id"]);
+    if (res["value"]) {
+      found = true;
     }
+  });
+  if (!found) {
+    $("#input_div_" + String(globalThis.counter - 1)).remove();
+    globalThis.counter -= 1;
   }
 }
 
@@ -133,11 +144,27 @@ function delete_input_if_necessary() {
 // the D score is calculated/updated.
 // empty slots will be removed if necessary
 function update_routine() {
-  element_description = $("#element_" + String(globalThis.counter - 1)).val();
-  res = find_element(element_description);
-  if (res["description"]) {
-    add_input();
-  }
+  add_input();
   update_d_score();
   delete_input_if_necessary();
+}
+
+function add_input() {
+  if (globalThis.counter > 1) {
+    selector_data = $("#selector_" + String(globalThis.counter - 1)).select2(
+      "data"
+    );
+    found = false;
+    selector_data.forEach((element) => {
+      res = find_element(element["id"]);
+      if (res["value"]) {
+        found = true;
+      }
+    });
+
+    if (found) {
+      div = $("#routine_placeholder");
+      create_datalist(div);
+    }
+  }
 }
