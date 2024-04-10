12abaa9cdbcc2ed8ba58929c71407c59776f9054
tbollen <thomas.bollen97@gmail.com>
2024-04-10 23:16:45 +0200

    Added routine builder framework

---
+++ src/routes/routine_builder/_components/rb_element.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let element = {};
+</script>
+
+<div class="wrapper">
+	<p id="number">{element.number}</p>
+	<h3 id="name">{element.description}</h3>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		background-color: darkblue;
+		padding: 0.2rem;
+		border-radius: var(--bc_border-radius);
+	}
+</style>
