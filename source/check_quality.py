998cd1ff1b9f9ab0f1085f3592db0a1e0cb9dd04
loesvandenbiggelaar <vdbiggelaarloes@gmail.com>
2024-04-10 23:07:14 +0200

    simple check to see how much is mapped. overall it's around 90% coverage.

--- source/check_quality.py
+++ source/check_quality.py
@@ -3,9 +3,9 @@ from functions import loadJson, loadConfig
 element_counts = loadConfig("source/nr_elements_check.yaml")
 
 gender = "men"
-language = "en"
+language = "nl"
 
-elements = loadJson(f"data/elements/{gender}/{language}_elements.json")
+elements = loadJson(f"src/lib/data/elements/{gender}/{language}_elements.json")
 
 for apparatus, items in elements.items():
-    print(apparatus, len(items))
+    print(apparatus, len(items),element_counts[gender][apparatus])
