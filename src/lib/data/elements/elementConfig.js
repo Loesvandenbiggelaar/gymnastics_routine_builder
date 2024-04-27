a46c9c29cf28a340291d6569dc44634be385d068
tbollen <thomas.bollen97@gmail.com>
2024-04-27 19:15:59 +0200

    Fixed typo in id

--- src/lib/data/elements/elementConfig.js
+++ src/lib/data/elements/elementConfig.js
@@ -18,31 +18,46 @@ export const placeholderElement = {
 // The dictionary that keeps track of the different apparatuses
 // In this new config, the difference between mens and womens is omitted, merging it into 1 dictionary (apart from overlapping apparatuses)
 export const apparatusConfig = {
-	v_w: { name: m.apparatus_vault(), icon: 'vault.svg', id: 'v_w', sex: m.sex_womens() },
-	b: { name: m.apparatus_beam(), icon: 'beam.svg', id: 'b', sex: m.sex_womens() },
+	v_w: {
+		name: m.apparatus_vault(),
+		icon: 'vault.svg',
+		id: 'v_w',
+		sex: m.sex_womens(),
+		sex_id: 'w'
+	},
+	b: { name: m.apparatus_beam(), icon: 'beam.svg', id: 'b', sex: m.sex_womens(), sex_id: 'w' },
 	ub: {
 		name: m.apparatus_uneven_bars(),
 		icon: 'uneven_bars.svg',
 		id: 'ub',
-		sex: m.sex_womens()
+		sex: m.sex_womens(),
+		sex_id: 'w'
 	},
-	f_w: { name: m.apparatus_floor(), icon: 'floor.svg', id: 'f_w', sex: m.sex_womens() },
-	v_m: { name: m.apparatus_vault(), icon: 'vault.svg', id: 'v_m', sex: m.sex_mens() },
-	r: { name: 'rings', icon: 'rings.svg', id: 'r', sex: m.sex_mens() },
+	f_w: {
+		name: m.apparatus_floor(),
+		icon: 'floor.svg',
+		id: 'f_w',
+		sex: m.sex_womens(),
+		sex_id: 'w'
+	},
+	v_m: { name: m.apparatus_vault(), icon: 'vault.svg', id: 'v_m', sex: m.sex_mens(), sex_id: 'm' },
+	r: { name: 'rings', icon: 'rings.svg', id: 'r', sex: m.sex_mens(), sex_id: 'm' },
 	ph: {
 		name: 'pommel horse',
 		icon: 'pommel_horse.svg',
 		id: 'ph',
-		sex: m.sex_mens()
+		sex: m.sex_mens(),
+		sex_id: 'm'
 	},
-	hb: { name: 'high bar', icon: 'high_bar.svg', id: 'hb', sex: m.sex_mens() },
+	hb: { name: 'high bar', icon: 'high_bar.svg', id: 'hb', sex: m.sex_mens(), sex_id: 'm' },
 	pb: {
 		name: 'parallel bars',
 		icon: 'parallel_bars.svg',
 		id: 'pb',
-		sex: m.sex_mens()
+		sex: m.sex_mens(),
+		sex_id: 'm'
 	},
-	f_m: { name: m.apparatus_floor(), icon: 'floor.svg', id: 'fm', sex: m.sex_mens() }
+	f_m: { name: m.apparatus_floor(), icon: 'floor.svg', id: 'f_m', sex: m.sex_mens(), sex_id: 'm' }
 };
 
 export const defaultApparatus = apparatusConfig.v_w;
