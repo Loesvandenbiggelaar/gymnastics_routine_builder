e812409888fd7bfb064bd17d985892d5f1b2a8a1
tbollen <thomas.bollen97@gmail.com>
2024-05-03 19:36:06 +0200

    fuck simple-svelte-datatables

--- _app/src/routes/builder/+page.svelte
+++ _app/src/routes/builder/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
 	// Import Components
 	import ApparatusPicker from '$lib/components/ApparatusPicker.svelte';
-	import Datatable from '$lib/components/datatable/Datatable.svelte';
 </script>
-
-<Datatable />
