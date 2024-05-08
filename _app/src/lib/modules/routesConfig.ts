e7217ff6e210ec8ced7a413a502a7bb250c62269
tbollen <thomas.bollen97@gmail.com>
2024-05-08 14:22:23 +0200

    added type to routes

--- _app/src/lib/modules/routesConfig.ts
+++ _app/src/lib/modules/routesConfig.ts
@@ -1,3 +1,10 @@
+export type Route = {
+	name: string;
+	href: string;
+	icon: string;
+	display?: boolean;
+};
+
 export const routes = [
 	{ name: 'Home', href: '/', icon: 'mdi:home', display: true },
 	{ name: 'About', href: '/about', icon: 'mdi:information-outline', display: true },
