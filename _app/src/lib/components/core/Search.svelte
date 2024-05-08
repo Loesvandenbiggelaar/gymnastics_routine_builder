dd8885ffe42eb5a65c6d4a183a6630231de64d0c
tbollen <thomas.bollen97@gmail.com>
2024-05-08 21:39:55 +0200

    Added basic support for searchList, which supports multiple search values!

--- _app/src/lib/components/core/Search.svelte
+++ _app/src/lib/components/core/Search.svelte
@@ -14,10 +14,15 @@
 	let searchProperties = $data.filterList.searchProperties || ['id', 'description', 'value'];
 
 	// MULTIPLE SEARCH PROPERTIES
+	//
+	const searchTagList = ['foo', 'bar', 'baz', 'chipmunk', 'quux'];
+
+	//
+	//
 
 	// Icon and default settings
 	let searchProperties_defaulted = (searchProperties = ['id', 'description', 'value']);
-	$: icon = searchProperties_defaulted ? 'mdi:filter-outline' : 'mdi:filter';
+	$: icon = searchProperties_defaulted ? 'mdi:tune-vertical-variant' : 'mdi:filter';
 
 	// Props list
 	let propsList: Record<keyof (typeof $data.elementData)[0] | string, boolean> = Object.keys(
@@ -41,20 +46,55 @@
 		$data.setSearchProperties(trueKeys);
 	}
 
+	function clearSearch() {
+		value = '';
+	}
+
+	function searchKeydown(e: KeyboardEvent) {
+		if (e.key === 'Delete') {
+			return clearSearch();
+		}
+		if (e.key === 'Enter') {
+			$data.filterList.searchList = [...$data.filterList.searchList, value] as string[];
+			clearSearch();
+			return;
+		}
+	}
+
 	// Call $data.search when value is changed
 	// But only AFTER $data is loaded
 </script>
 
 <div id="search" class="w-fit relative">
-	<input type="text" bind:value {placeholder} class="input input-bordered w-full" />
-	<div
-		id="filter-icons"
-		class="btn hover:variant-filled-surface"
+	<div id="searchBar">
+		<input
+			id="searchInput"
+			type="text"
+			bind:value
+			{placeholder}
+			class="input input-bordered w-full"
+			on:keydown={searchKeydown}
+		/>
+		<div id="searchBarButtons">
+			{#if value !== ''}
+				<button id="clearSearch" class="btn hover:variant-ghost-surface" on:click={clearSearch}>
+					<Icon icon="mdi:close" />
+				</button>
+			{:else}
+				<button on:click={() => document.getElementById('searchInput')?.focus()}>
+					<Icon icon="mdi:magnify" />
+				</button>
+			{/if}
+		</div>
+	</div>
+	<button
+		id="searchFilter"
+		class="btn"
 		class:inactive={searchProperties_defaulted}
 		use:popup={popupSettings}
 	>
 		<Icon {icon} />
-	</div>
+	</button>
 </div>
 
 <div id="searchPopup" class="card p-2 z-10" data-popup={popupSettings.target}>
@@ -73,16 +113,33 @@
 </div>
 
 <style>
-	#filter-icons {
+	#search {
+		display: flex;
+	}
+
+	#searchBar {
+		position: relative;
+	}
+
+	#searchBarButtons {
 		position: absolute;
-		padding: 0.5em;
-		right: 0.5em;
+		align-items: center;
+		right: 0.6em;
 		top: 50%;
 		transform: translateY(-50%);
 		font-size: 1em;
 	}
 
+	#searchBarButtons button {
+		padding: 0.3em;
+	}
+
 	.inactive:not(:hover) {
 		opacity: 0.5;
+	}
+
+	#searchFilter {
+		padding: 0.3em;
+		aspect-ratio: 1;
 	}
 </style>
