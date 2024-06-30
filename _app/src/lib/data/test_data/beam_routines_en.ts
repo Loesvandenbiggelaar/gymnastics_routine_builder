a9e92054abc6c3c3787ed9686700a21f7f342b73
Loes van den Biggelaar <loes@thehyve.nl>
2024-06-30 18:10:29 +0200

    example beam routine

---
+++ _app/src/lib/data/test_data/beam_routines_en.ts
@@ -0,0 +1,246 @@
+export const beamRoutine1 = [
+    {
+        "elements": [
+            {
+                "element": {
+                    "id": "1.101",
+                    "description": "(D) Leap - on landing must show arabesque position (leg min. at horizontal)",
+                    "group_number": "1",
+                    "group_name": "Mounts",
+                    "difficulty": "A",
+                    "type": "dance",
+                    "value": 0.1,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page108_element_1.101_Im903.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page108_element_1.101_Im904.png"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    {
+        "elements": [
+            {
+                "element": {
+                    "id": "2.303",
+                    "description": "Straddle pike jump with ½ turn (180°) from cross position",
+                    "group_number": "2",
+                    "group_name": "Gymnastic leaps, jumps and hops",
+                    "difficulty": "C",
+                    "type": "dance",
+                    "value": 0.3,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page121_element_2.303_Im1106.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page121_element_2.303_Im1102.png"
+                        ]
+                    }
+                }
+            },
+            {
+                "element": {
+                    "id": "2.101",
+                    "description": "Split leap fwd (leg separation 180°)",
+                    "group_number": "2",
+                    "group_name": "Gymnastic leaps, jumps and hops",
+                    "difficulty": "A",
+                    "type": "dance",
+                    "value": 0.1,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page120_element_2.101_Im1093.png",
+                            "/data/elements/women//images/beam/element\\page120_element_2.101_Im1098.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page120_element_2.101_Im1099.png"
+                        ]
+                    }
+                }
+            },
+            {
+                "element": {
+                    "id": "2.303",
+                    "description": "Straddle pike jump with ½ turn (180°) from cross position",
+                    "group_number": "2",
+                    "group_name": "Gymnastic leaps, jumps and hops",
+                    "difficulty": "C",
+                    "type": "dance",
+                    "value": 0.3,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page121_element_2.303_Im1106.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page121_element_2.303_Im1102.png"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    {
+        "elements": [
+            {
+                "element": {
+                    "id": "2.310",
+                    "description": "Cat leap with 1/1 turn (360°)",
+                    "group_number": "2",
+                    "group_name": "Gymnastic leaps, jumps and hops",
+                    "difficulty": "C",
+                    "type": "dance",
+                    "value": 0.3,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page125_element_2.310_Im1160.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page125_element_2.310_Im1161.png"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    {
+        "elements": [
+            {
+                "element": {
+                    "id": "5.312",
+                    "description": "Salto bwd tucked, piked or stretched (step out)",
+                    "group_number": "5",
+                    "group_name": "Acrobatic flight",
+                    "difficulty": "C",
+                    "type": "acrobatic",
+                    "value": 0.3,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page140_element_5.312_Im1378.png",
+                            "/data/elements/women//images/beam/element\\page140_element_5.312_Im1377.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page140_element_5.312_Im1382.png",
+                            "/data/elements/women//images/beam/symbol\\page140_element_5.312_Im1384.png"
+                        ]
+                    }
+                }
+            },
+            {
+                "element": {
+                    "id": "5.312",
+                    "description": "Salto bwd tucked, piked or stretched (step out)",
+                    "group_number": "5",
+                    "group_name": "Acrobatic flight",
+                    "difficulty": "C",
+                    "type": "acrobatic",
+                    "value": 0.3,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page140_element_5.312_Im1378.png",
+                            "/data/elements/women//images/beam/element\\page140_element_5.312_Im1377.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page140_element_5.312_Im1382.png",
+                            "/data/elements/women//images/beam/symbol\\page140_element_5.312_Im1384.png"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    {
+        "elements": [
+            {
+                "element": {
+                    "id": "4.107",
+                    "description": "Cartwheel, also with support on one arm, or Cartwheel with flight phase before or after hand support",
+                    "group_number": "4",
+                    "group_name": "Holds and acrobatic non-flight",
+                    "difficulty": "A",
+                    "type": "acrobatic",
+                    "value": 0.1,
+                    "images": {
+                        "figure": [],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1268.png",
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1267.png",
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1257.png",
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1259.png",
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1258.png"
+                        ]
+                    }
+                }
+            },
+            {
+                "element": {
+                    "id": "5.201",
+                    "description": "Handspring fwd with flight to land on one or both legs (same element), also with support on one arm",
+                    "group_number": "5",
+                    "group_name": "Acrobatic flight",
+                    "difficulty": "B",
+                    "type": "acrobatic",
+                    "value": 0.2,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page135_element_5.201_Im1289.png",
+                            "/data/elements/women//images/beam/element\\page135_element_5.201_Im1305.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page135_element_5.201_Im1306.png",
+                            "/data/elements/women//images/beam/symbol\\page135_element_5.201_Im1304.png"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    {
+        "elements": [
+            {
+                "element": {
+                    "id": "4.107",
+                    "description": "Cartwheel, also with support on one arm, or Cartwheel with flight phase before or after hand support",
+                    "group_number": "4",
+                    "group_name": "Holds and acrobatic non-flight",
+                    "difficulty": "A",
+                    "type": "acrobatic",
+                    "value": 0.1,
+                    "images": {
+                        "figure": [],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1268.png",
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1267.png",
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1257.png",
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1259.png",
+                            "/data/elements/women//images/beam/symbol\\page133_element_4.107_Im1258.png"
+                        ]
+                    }
+                }
+            },
+            {
+                "element": {
+                    "id": "6.204",
+                    "description": "Salto bwd tucked or stretched with 1/1 twist (360°)",
+                    "group_number": "6",
+                    "group_name": "Dismounts",
+                    "difficulty": "B",
+                    "type": "acrobatic",
+                    "value": 0.2,
+                    "images": {
+                        "figure": [
+                            "/data/elements/women//images/beam/element\\page143_element_6.204_Im1435.png"
+                        ],
+                        "symbol": [
+                            "/data/elements/women//images/beam/symbol\\page143_element_6.204_Im1436.png"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+]
