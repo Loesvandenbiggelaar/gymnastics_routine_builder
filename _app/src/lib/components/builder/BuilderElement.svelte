4c21e2872c7c2c5873a7163431ae7683a1622fc8
tbollen <thomas.bollen97@gmail.com>
2024-07-01 16:54:38 +0200

    GUI of Routine Builder

--- _app/src/lib/components/builder/BuilderElement.svelte
+++ _app/src/lib/components/builder/BuilderElement.svelte
@@ -4,27 +4,126 @@
 	export let element: ElementMetadata;
 	export let elementIndex: number;
 	export let comboIndex: number;
-	import {routineBuilder} from '$lib/stores/routineBuilder'
+	import { routineBuilder } from '$lib/stores/routineBuilder';
 	import type { ElementMetadata } from '$lib/stores/routineBuilder';
-	
+	import { onMount } from 'svelte';
 
-	function deleteElementFromCombo() {$routineBuilder.removeElement(comboIndex,elementIndex)}
-	function moveElementUp(){
-		console.log(comboIndex, elementIndex)
-		$routineBuilder.moveElementBack(comboIndex,elementIndex)
+	function deleteElementFromCombo() {
+		$routineBuilder.removeElement(comboIndex, elementIndex);
+	}
+	function moveElementUp() {
+		console.log(comboIndex, elementIndex);
+		$routineBuilder.moveElementBack(comboIndex, elementIndex);
 	}
 
+	$: dragging = false;
+	function isDragging() {
+		dragging = true;
+	}
+
+	let mouse = { x: 0, y: 0 };
+
+	function mouseMove(e: MouseEvent) {
+		mouse = { x: e.clientX, y: e.clientY };
+	}
+
+	onMount(() => {
+		document.addEventListener('mouseup', mouseRelease);
+	});
+
+	function mouseRelease() {
+		if (!dragging) return;
+		dragging = false;
+	}
 </script>
 
-<div class="element">
-	<div class="element-description">{element.element.description}</div>
-	<button id="deleteElement" class="btn" on:click={deleteElementFromCombo}
-		><Icon icon="mdi:delete" /></button
-	>
-	<button id="moveElementUp" class="btn" on:click={moveElementUp}
-		><Icon icon="mdi:arrow-up" /></button
-	>
-	<!-- <button id="moveElementUp" class="btn" on:click={moveElementUp}
-	><Icon icon="mdi:arrow-down" /></button
-> -->
+<div
+	class="routineBuilderElement bg-surface-100-800-token text-surface-900-50-token"
+	class:dragging
+>
+	<div class="elementHead">
+		<div class="announcer">element</div>
+	</div>
+	<div class="elementDescription">{element.element.description}</div>
+	<!-- Drag Handle -->
+	<button id="draghandle" class="dragHandle btn" on:mousedown={isDragging}>
+		<Icon icon="mdi:drag" />
+	</button>
+	<!-- Buttons -->
+	<div class="rbeButtons">
+		<button id="deleteElement" class="btn" on:click={deleteElementFromCombo}
+			><Icon icon="mdi:delete" /></button
+		>
+		<button id="moveElementUp" class="btn" on:click={moveElementUp}
+			><Icon icon="mdi:arrow-up" /></button
+		>
+		<button id="moveElementUp" class="btn" on:click={moveElementUp}
+			><Icon icon="mdi:arrow-down" /></button
+		>
+	</div>
 </div>
+
+<style>
+	.routineBuilderElement {
+		display: grid;
+		grid-template-areas:
+			'head head head'
+			'something description draghandle'
+			'buttons buttons buttons';
+		grid-template-columns: min-content 1fr min-content;
+
+		/* Layout */
+		border-radius: inherit;
+		width: 100%;
+		padding: 0.5em;
+
+		/* Animation */
+		transition: all 0.2s ease-in-out;
+	}
+
+	.routineBuilderElement:hover {
+		box-shadow: 0 2px 2px 2px rgba(27, 66, 117, 0.747);
+	}
+
+	.routineBuilderElement.dragging {
+		opacity: 0.5;
+		animation: wobble 0.5s ease infinite;
+	}
+
+	.elementHead {
+		grid-area: head;
+		border-bottom: 0.5px solid rgb(var(--color-surface-500));
+	}
+
+	.elementHead .announcer {
+		font-size: 9pt;
+		font-weight: 900;
+		font-family: 'Kode Mono', monospace;
+	}
+
+	.elementDescription {
+		grid-area: description;
+	}
+
+	.dragHandle {
+		grid-area: draghandle;
+	}
+
+	.rbeButtons {
+		display: flex;
+		grid-area: buttons;
+	}
+
+	@keyframes wobble {
+		0%,
+		100% {
+			transform: rotate(0);
+		}
+		33% {
+			transform: rotate(-0.5deg);
+		}
+		66% {
+			transform: rotate(0.5deg);
+		}
+	}
+</style>
--- _app/src/routes/builder/+page.svelte
+++ _app/src/routes/builder/+page.svelte
@@ -5,11 +5,15 @@
 
 	import { createRoutine, getElement } from '$lib/stores/debug_get_routines';
 
-	import {routineBuilder} from '$lib/stores/routineBuilder'
+	import { routineBuilder } from '$lib/stores/routineBuilder';
 	// console.log("my routine", routineBuilder.getRoutineIds())
 	import BuilderElement from '$lib/components/builder/BuilderElement.svelte';
-	console.log("default:routine", $routineBuilder.routine)
+	console.log('default:routine', $routineBuilder.routine);
 
+	// import test data
+	import { beamRoutine1 } from '$lib/data/test_data/beam_routines_en';
+
+	$: testRoutine = beamRoutine1;
 
 	let inputValue = '';
 
@@ -18,47 +22,54 @@
 		console.log('Input value:', inputValue);
 		// Call your function with the input value
 		// console(inputValue);
-		$routineBuilder.addElement(getElement("b", inputValue))
-		console.log("routine", $routineBuilder.getRoutineIds())
-		$routineBuilder.calcDiff()
+		$routineBuilder.addElement(getElement('b', inputValue));
+		console.log('routine', $routineBuilder.getRoutineIds());
+		$routineBuilder.calcDiff();
 	}
 
-	function deleteButton(){
-		console.log($routineBuilder.routine)
-		console.log("deleting")
-		$routineBuilder.empty()
-		console.log($routineBuilder.routine)
+	function deleteButton() {
+		console.log($routineBuilder.routine);
+		console.log('deleting');
+		$routineBuilder.empty();
+		console.log($routineBuilder.routine);
 	}
 
-$: console.log($routineBuilder.routine)
-// $: $routineBuilder.calcDiff()
-$: console.log("total", $routineBuilder.difficulty.totalDifficulty)
+	$: console.log($routineBuilder.routine);
+	// $: $routineBuilder.calcDiff()
+	$: console.log('total', $routineBuilder.difficulty.totalDifficulty);
 </script>
 
 <!-- Search and Filter -->
 <!-- Builder -->
 
-
-
-<div class="builderWrapper">
-	{#each $routineBuilder.routine as combo, comboIndex}
-		<!-- Combo -->
-		<div class="comboWrapper" id="combo-{comboIndex}">
-			{#if combo.elements.length > 0}
-				{#each combo.elements as element, elementIndex}
-					{#if combo.elements.length > 0}
-						<BuilderElement {element} {elementIndex} {comboIndex} />
-					{:else}
-						<!-- NO ELEMENTS IN THIS COMBO -->
-					{/if}
-				{/each}
-			{:else}
-				<!-- When no combo's are found -->
-				<!-- No combo -->
-			{/if}
-		</div>
-	{/each}
-</div>
+<section id="builder">
+	<div class="builderHead">
+		<div class="name">Routine Name</div>
+	</div>
+	<div class="builderWrapper">
+		{#each testRoutine as combo, comboIndex}
+			<!-- Combo -->
+			<div class="comboWrapper bg-primary-500 text-white" id="combo-{comboIndex}">
+				<div class="comboHeader">
+					<div class="announcer">Combo {comboIndex + 1}</div>
+					<div class="buttons">buttons</div>
+				</div>
+				{#if combo.elements.length > 0}
+					{#each combo.elements as element, elementIndex}
+						{#if combo.elements.length > 0}
+							<BuilderElement {element} {elementIndex} {comboIndex} />
+						{:else}
+							<!-- NO ELEMENTS IN THIS COMBO -->
+						{/if}
+					{/each}
+				{:else}
+					<!-- When no combo's are found -->
+					<!-- No combo -->
+				{/if}
+			</div>
+		{/each}
+	</div>
+</section>
 
 <!-- Input -->
 <input type="text" bind:value={inputValue} style="color: black;" />
@@ -66,12 +77,51 @@ $: console.log("total", $routineBuilder.difficulty.totalDifficulty)
 <!-- Button to trigger the function -->
 <button on:click={processInput}>Add</button>
 
-<br>
+<br />
 
 <!-- Big Delete Button -->
 <button class="deleteButton" on:click={deleteButton}>Delete Routine</button>
 
-<br>
-<br>
+<br />
+<br />
 
 difficulty: {$routineBuilder.difficulty.difficultyElements}
+
+<style>
+	#builder {
+		padding: 1em 0px;
+		/* Max width */
+		max-width: 240mm;
+		margin: 0 auto;
+	}
+	.builderWrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 20px;
+	}
+	.comboWrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		gap: 10px;
+		/* Styling */
+		padding: 0.5em;
+		border-radius: 1em;
+	}
+
+	.comboHeader {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		/* Styling */
+		padding: 0.3em;
+		/* Text */
+		font-weight: bold;
+	}
+</style>
