e0ba496a4a6b2ff5fd4ad821ef89bb4179ad1221
Loes van den Biggelaar <loes@thehyve.nl>
2024-07-13 15:21:06 +0200

    remove console.error() and redundant file

--- _app/src/routes/+page.svelte
+++ _app/src/routes/+page.svelte
@@ -2,17 +2,7 @@
 	// Import Components
 	import Card from '$lib/components/core/Card.svelte';
 	import { cardItems } from '$lib/modules/cardItems';
-	import { allElements } from '$lib/data/elements/all_elements';
 	import '$lib/themes/vars.css';
-
-	console.error('allElements', allElements[Object.keys(allElements)[0]]);
-
-	import { data } from '$lib/stores/datastore';
-	console.error('data', $data.rawData);
-	console.error('language = ', $data.selectedLanguage);
-	console.error('language data', $data.apparatusData);
-	console.error('apparatus = ', $data.selectedApparatus);
-	console.error('apparatus data', $data.elementData);
 </script>
 
 <svelte:head>
