11c202ba1707d721c9d77fbe7fe9ea63a469621e
tbollen <thomas.bollen97@gmail.com>
2024-10-20 19:36:27 +0200

    svelte config changes

--- _app/svelte.config.js
+++ _app/svelte.config.js
@@ -18,8 +18,12 @@ const config = {
 		appDir: 'src',
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			assets: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+		fallback: 'index.html',
+		precompress: true,
+		assetPrefix: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 	}
 };
 export default config;
