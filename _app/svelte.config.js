e4009abb97056061f13a414750612e672ec438de
tbollen <thomas.bollen97@gmail.com>
2024-10-20 19:30:10 +0200

    removed prerender

--- _app/svelte.config.js
+++ _app/svelte.config.js
@@ -19,10 +19,6 @@ const config = {
 		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
-		prerender: {
-			enabled: true,
-			handleHttpError: 'warn'
 		}
 	}
 };
